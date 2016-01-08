2b970a106873a1b3c9698818c25f2820582de0f3
Isamu Mogi <isamu@leafytree.jp>
2016-01-08 23:29:48 +0900

    wip

--- stackw.bat
+++ stackw.bat
@@ -1,1 +1,9 @@
-echo "Not implemented yet"
+@set @x=0 /* -*- mode: javascript, coding: us-ascii-dos -*- vim: set ft=javascript:
+@cscript //Nologo //E:JScript "%~f0" %*
+@exit /b %errorlevel%
+
+  stack wrapper
+
+*/
+
+WScript.Echo("Not implemented yet");
